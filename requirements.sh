d02cbbaaee55e6b9f6a8b427f573ff9c0a8725db
avatarking <54052159+avatarking@users.noreply.github.com>
2019-08-13 18:16:25 +0700

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -14,7 +14,7 @@ apt install ruby -y
 gem install lolcat
 figlet -f big Done !!! | lolcat
 echo
-echo -e "\e[1m Now Run \e[32mzsh t-ban.sh\e[0m...!!!"
+echo -e "\e[1m Now Run \e[32mbash banner.sh\e[0m...!!!"
 echo
 echo -e "\e[1m\e[32m Developed by :\e[33m avatar king (atk)"
 echo
