b6f1ee13ae685f71f743843b69a2e5544c1eb8f4
avatarking <54052159+avatarking@users.noreply.github.com>
2019-08-13 12:56:36 +0700

    Add files via upload

---
+++ k.sh
@@ -0,0 +1,28 @@
+
+#
+#
+#scrip by avatarking(TH)
+# Termux Banner os
+# Do Not Copy Or Modify
+#
+
+clear
+figlet -f mono12  Banner | lolcat
+echo ππππππππππππππππππππππππππππππ| lolcat
+echo  "By Avatarking" 
+echo " IT FTOSJ"
+echo ππππππππππππππππππππππππππππππ| lolcat
+echo 
+echo 
+echo -
+echo
+echo What is Your screefetchName | lolcat
+read varscreenfetch
+echo  "screenfetch -A "$varscreenfetch> C0w54y.txt
+echo 
+echo
+echo "clear" > cl34r.txt
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+
+
